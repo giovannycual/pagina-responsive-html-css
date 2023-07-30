86b6e02652a03327fa1c9dc12480c534389e2264
giovanny <giovanny_cual@hotmail>
2023-07-30 16:45:55 -0500

    mi primer commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+let menu =document.querySelector('#menu-icon');
+let navlist =document.querySelector('.navlist');
+
+menu.onclick= () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+};
+
+const sr=ScrollReveal({
+    distance: '65px',
+    duration:2600,
+    delay:440,
+    reset:true
+});
+sr.reveal('.hero-text',{delay:200, origin:'top'})
+sr.reveal('.hero-img',{delay:350, origin:'top'})
+sr.reveal('.icons',{delay:400, origin:'top'})
+sr.reveal('.scroll-down',{delay:450, origin:'top'})
